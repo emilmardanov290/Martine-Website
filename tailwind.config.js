5f826146938fcdc2b79a904e00a3a4c189b8cd22
Emil Mardanov <emardanov21@gmail.com>
2025-09-28 19:10:08 +0400

    Первый коммит

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.htm"], 
+    theme: {
+    extend: {
+        screens: {
+        '3xl': '1920px',
+        '4xl': '2200px',
+      },
+    },
+    },
+    plugins: [],
+}
